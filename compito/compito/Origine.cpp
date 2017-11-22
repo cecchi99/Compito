c3c265d1cf696a9033dc5927a98bc57e903b103d
cecchi99 <andreacecchi4@gmail.com>
2017-11-22 09:32:32 +0100

    documentazione master

--- compito/compito/Origine.cpp
+++ compito/compito/Origine.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 using namespace std;
 
+/*
+@brief Programma che incrementa una variabile somma e la stampa
+@fn void main()
+@warning Sono sul branch master
+*/
 void main()
 {
-	int conta = 0;
+	int conta = 0;//variabile conta
 
+	//ciclo di incremento della variabile conta
 	do {
 		conta++;
 		cout << "Conta: " << conta << endl;
