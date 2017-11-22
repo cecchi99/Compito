fc1eec42795693f2f0bb2d0b5ac5d69fd3f650c4
cecchi99 <andreacecchi4@gmail.com>
2017-11-22 09:30:04 +0100

    commenti su FIX_BUG

--- compito/compito/Origine.cpp
+++ compito/compito/Origine.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+/*
+@brief Programma che incrementa una variabile conta e la stampa
+@fn void main()
+@author Andrea Cecchi 5BIA
+@date 22/11/2017
+@file Origine.cpp
+@warning Sono sul branch FIX_BUG
+*/
 void main()
 {
 	int conta = 0;
